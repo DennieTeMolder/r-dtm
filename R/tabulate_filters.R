602f7c19e8161f40c61281331f720b43eaac1991
DennieTeMolder <dennie503@live.nl>
2023-02-09 12:05:08 +0100

    tabulate_filters: rename output df cols

--- R/tabulate_filters.R
+++ R/tabulate_filters.R
@@ -8,22 +8,22 @@ tabulate_filters <- function(df, cutoffs) {
     min <- cutoffs[[current]]$min
     if (!is.null(min)) {
       result <- rbind(result, data.frame(
-        filter = paste(current, ">=", min),
-        n_fail = sum(df[[current]] < min, na.rm = TRUE)
+        filter = paste(current, "<", min),
+        n_removed = sum(df[[current]] < min, na.rm = TRUE)
       ))
     }
 
     max <- cutoffs[[current]]$max
     if (!is.null(max)) {
       result <- rbind(result, data.frame(
-        filter = paste(current, "<=", max),
-        n_fail = sum(df[[current]] > max, na.rm = TRUE)
+        filter = paste(current, ">", max),
+        n_removed = sum(df[[current]] > max, na.rm = TRUE)
       ))
     }
   }
 
-  result <- result[order(result$n_fail, decreasing = TRUE), ]
-  result$frac = round(result$n_fail / nrow(df), digits = 3)
+  result <- result[order(result$n_removed, decreasing = TRUE), ]
+  result$frac = round(result$n_removed / nrow(df), digits = 3)
 
   result
 }

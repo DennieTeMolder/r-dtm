b066aa954c939d1450214b9f07fec4ee2cee3dec
DennieTeMolder <dennie503@live.nl>
2023-02-10 17:20:16 +0100

    get_oldest_parent_info: export function

--- R/get_oldest_parent.R
+++ R/get_oldest_parent.R
@@ -1,16 +1,16 @@
 # Specifically designed for WN metadata xlsx files
 ##' @export
 get_oldest_parent <- function(df, codes = NULL) {
+  stopifnot(is.data.frame(df))
   stopifnot(c("population_code", "parental_population_code") %in% colnames(df))
 
   if (is.null(codes))
     codes <- df$population_code
 
   idx <- match(codes, df$population_code)
-  if (any(is.na(idx))) {
-    missing <- paste(codes[is.na(idx)], collapse = ", ")
-    stop(paste("Cannot find population codes:", missing))
-  }
+  if (any(is.na(idx)))
+    stop("Cannot find population codes:", collapse(codes[is.na(idx)]))
+
 
   # Fetch all parental codes, when missing use current code
   parents <- df$parental_population_code[idx]
@@ -23,8 +23,8 @@ get_oldest_parent <- function(df, codes = NULL) {
   parents
 }
 
+##' @export
 get_oldest_parent_info <- function(df, codes = NULL) {
   parents <- get_oldest_parent(df = df, codes = codes)
-  idx <- match(parents, df$population_code)
-  return(df[idx,])
+  semi_join_reorder(df, parents, by = "population_code")
 }

3897f071e02dd7d2016c123a60d30ff67158412d
DennieTeMolder <dennie503@live.nl>
2023-02-27 12:53:56 +0100

    semi_join_reorder:  check arguments

--- R/semi_join_reorder.R
+++ R/semi_join_reorder.R
@@ -1,18 +1,30 @@
 # return all rows in `x` with match in `y` in the order of `y`
 ##' @export
 semi_join_reorder <- function(x, y, by, ...) {
-  if (is.vector(y) && length(by) == 1 && is.null(names(by))) {
-    y <- tibble::tibble(y1 = y)
+  stopifnot(is.data.frame(x))
+  stopifnot(is.data.frame(y) || is.vector(y))
+  stopifnot(is.character(by))
+  stopifnot(by %in% colnames(y))
+
+  has_names <- !is.null(names(by))
+
+  if (is.vector(y)) {
+    if (length(by) != 1 || has_names)
+      stop("When 'y' is a vector, 'by' should be a single value unnamed character vector!")
+    y <- tibble::tibble(y)
     colnames(y) <- by
   } else {
-    y <- y[, by]
+    y <- y[, by, drop = FALSE]
   }
-  if (!is.null(names(by))) {
+
+  # Rename columns of 'y' to be identical to 'x'
+  if (has_names) {
     dict <- names(by)
     names(dict) <- by
     y <- dplyr::rename_with(y, .lookup_maybe, dict = dict)
     by <- names(by)
   }
+
   args <- list(x = y, y = x, by = by, ...)
   do.call(dplyr::left_join, args)
 }

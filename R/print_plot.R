adab078f80dd174c0f3cb4ce90ab3d1ee4afbb47
DennieTeMolder <dennie503@live.nl>
2023-03-24 04:45:53 +0100

    print_plot: change plot dir

--- R/print_plot.R
+++ R/print_plot.R
@@ -2,25 +2,29 @@
 # Print and save plot to filename at the same time unless `option(inhibit_save = TRUE)`.
 # If `option(print2pdf = TRUE)` a pdf is written to `tempdir()` instead of printing.
 print_plot <- function(p,
-                       width = 5,
-                       height = 4,
+                       width = 7,
+                       height = 5,
                        filename = NULL,
                        path = NULL,
                        ...) {
   print2pdf <- getOption("print2pdf", default = FALSE)
-  do_save <- !(is.null(filename) || getOption("inhibit_save", default = FALSE))
 
   if (print2pdf) {
-    dir <- tempdir(check = TRUE)
-    n <- length(list.files(dir, pattern = "emacs_plot_\\d{3}.pdf"))
-    emacs_filename <- sprintf("emacs_plot_%03d.pdf", n)
+    dir <- paste0(tempdir(check = TRUE), "/emacs_plots")
+    n <- length(list.files(dir, pattern = "\\d{3}.pdf"))
+    emacs_filename <- sprintf("%03d.pdf", n)
     emacs_file <- ggplot2::ggsave(emacs_filename, plot = p, path = dir,
                                   width = width, height = height, ...)
   } else {
     print(p)
   }
 
-  if (do_save) {
+  if (is.null(filename))
+    return(invisible(p))
+
+  if (getOption("inhibit_save", default = FALSE)) {
+    message("Saving to file inhibited.")
+  } else {
     if (print2pdf && stringr::str_ends(filename, "\\.pdf")) {
       file.copy(emacs_file, filename, overwrite = TRUE)
     } else {

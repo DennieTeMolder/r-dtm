3cc0b7826ec96269c6a63d71e293d890c53051fe
DennieTeMolder <dennie503@live.nl>
2023-06-23 15:25:47 +0200

    read_xlsx: read all sheets by default

--- R/read_xlsx.R
+++ R/read_xlsx.R
@@ -1,4 +1,9 @@
 ##' @export
 read_xlsx <- function(file, na = c("", "NA"), ...) {
-  readxl::read_excel(file, na = na, ...)
+  sheets <- readxl::excel_sheets(file)
+  f <- function(sheet)
+    readxl::read_excel(file, sheet = sheet, na = na, ...)
+  if (length(sheets) < 2)
+    return(f(sheets))
+  sapply(sheets, f, simplify = FALSE, USE.NAMES = TRUE)
 }

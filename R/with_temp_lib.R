6ec1253512f7a008686fccfada94d56d00fb0f0e
DennieTeMolder <dennie503@live.nl>
2023-08-03 17:23:49 +0200

    with_temp_lib: make location user specifiable

--- R/with_temp_lib.R
+++ R/with_temp_lib.R
@@ -1,8 +1,8 @@
 ##' @export
 # Example: with_temp_lib(BiocManager::install("example_package"))
 # OR with_temp_lib(remotes::install_cran("example_package"))
-with_temp_lib <- function(expr) {
-  .libPaths(unique(c(tempdir(), .libPaths())))
-  on.exit(.libPaths(c(.libPaths()[-1], tempdir())))
+with_temp_lib <- function(expr, lib = tempdir()) {
+  .libPaths(unique(c(lib, .libPaths())))
+  on.exit(.libPaths(c(.libPaths()[.libPaths() != lib], lib)))
   invisible(eval(expr))
 }

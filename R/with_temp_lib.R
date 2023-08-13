501403e2bdc58f318beb3969372aa89d0ef4addd
DennieTeMolder <dennie503@live.nl>
2023-08-13 22:14:04 +0200

    add_lib_path: new function

--- R/with_temp_lib.R
+++ R/with_temp_lib.R
@@ -2,7 +2,23 @@
 # Example: with_temp_lib(BiocManager::install("example_package"))
 # OR with_temp_lib(remotes::install_cran("example_package"))
 with_temp_lib <- function(expr, lib = tempdir()) {
-  .libPaths(unique(c(lib, .libPaths())))
-  on.exit(.libPaths(c(.libPaths()[.libPaths() != lib], lib)))
+  if (!file.exists(lib)) {
+    cli::cli_alert_info(paste("Creating directory:", lib))
+    dir.create(lib, recursive = TRUE)
+  }
+  add_lib_path(lib, append = FALSE)
+  on.exit(add_lib_path(lib, append = TRUE))
   invisible(eval(expr))
+}
+
+##' @export
+add_lib_path <- function(lib, append = TRUE) {
+  stopifnot(file.exists(lib))
+  old <- .libPaths()
+  if (append) {
+    new <- c(old[!old %in% lib], lib)
+  } else {
+    new <- unique(c(lib, old))
+  }
+  .libPaths(new)
 }

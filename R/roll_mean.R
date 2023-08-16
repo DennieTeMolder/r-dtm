1068b55da1197579471f7d7c9a40cfa060e64ec4
DennieTeMolder <dennie503@live.nl>
2023-08-16 10:09:05 +0200

    roll_mean: fix typo

--- R/roll_mean.R
+++ R/roll_mean.R
@@ -2,7 +2,7 @@
 ##' @export
 roll_mean <- function(x, k, silent = FALSE, ...) {
   if (!silent && k %% 2 == 0)
-    warning("It is recommended to use on uneven size for 'k'.")
+    warning("It is recommended to use an uneven size for 'k'.")
   k <- k / 2
   slider::slide_dbl(x, mean, ..., .before = ceiling(k) - 1, .after = floor(k))
 }

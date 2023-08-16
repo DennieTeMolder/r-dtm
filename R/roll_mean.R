b0998632fe7c057d6d45b4a39137958d30c7a3f1
DennieTeMolder <dennie503@live.nl>
2023-08-16 10:08:13 +0200

    roll_mean: fix 'x' not being used

--- R/roll_mean.R
+++ R/roll_mean.R
@@ -4,5 +4,5 @@ roll_mean <- function(x, k, silent = FALSE, ...) {
   if (!silent && k %% 2 == 0)
     warning("It is recommended to use on uneven size for 'k'.")
   k <- k / 2
-  slider::slide_dbl(1:10, mean, ..., .before = ceiling(k) - 1, .after = floor(k))
+  slider::slide_dbl(x, mean, ..., .before = ceiling(k) - 1, .after = floor(k))
 }

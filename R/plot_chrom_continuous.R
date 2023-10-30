e56066b14b1c4b5b6e10510f2e56a9828767e6e2
DennieTeMolder <dennie503@live.nl>
2023-10-30 15:54:00 +0100

    plot_chrom_continuous: minor code simplification

--- R/plot_chrom_continuous.R
+++ R/plot_chrom_continuous.R
@@ -9,7 +9,7 @@ plot_chrom_continuous <- function(df, aes = NULL, sizes = NULL) {
   stopifnot(is.numeric(df$pos), !is.na(df$pos))
   if (!is.null(sizes)) {
     stopifnot(is.data.frame(sizes), dplyr::between(ncol(sizes), 2, 3))
-    stopifnot(is.character(sizes[[1]]), !duplicated(sizes[[1]]))
+    stopifnot(is.character(sizes[[1]]), !anyDuplicated(sizes[[1]]))
     stopifnot(is.numeric(sizes[[2]]))
     if (ncol(sizes) == 3)
       stopifnot(is.character(sizes[[3]]))
@@ -25,19 +25,19 @@ plot_chrom_continuous <- function(df, aes = NULL, sizes = NULL) {
     sizes <- dplyr::summarise(dplyr::group_by(df, .data$chrom), len = max(.data$pos))
   } else {
     colnames(sizes)[1:2] <- c("chrom", "len")
-    sizes <- dplyr::filter(sizes, .data$chrom %in% used_chroms)
+    sizes <- dplyr::filter(dplyr::ungroup(sizes), .data$chrom %in% used_chroms)
   }
 
   # Compute position adjustment
-  size_order <- order(sizes$len, decreasing = TRUE)
-  adjust_pos <- cumsum(sizes$len[size_order])
-  adjust_pos <- c(0, utils::head(adjust_pos, n = -1))
-  names(adjust_pos) <- sizes$chrom[size_order]
+  sizes <- dplyr::arrange(sizes, -len)
+  adjust_pos <- cumsum(utils::head(sizes$len, n = -1))
+  adjust_pos <- c(0, adjust_pos)
+  names(adjust_pos) <- sizes$chrom
   df$.pos_adj <- df$pos + adjust_pos[df$chrom]
 
-  breaks <- adjust_pos
+  breaks <- c(adjust_pos, sum(sizes$len))
   if (ncol(sizes) == 3)
-    names(breaks) <- sizes[[3]][size_order]
+    names(breaks) <- c(sizes[[3]], "")
 
   # Modify aesthetic mappings
   if (is.null(aes)) {
@@ -49,6 +49,6 @@ plot_chrom_continuous <- function(df, aes = NULL, sizes = NULL) {
   # Plot
   ggplot2::ggplot(df, aes) +
     ggplot2::scale_x_continuous(breaks = breaks, minor_breaks = NULL) +
-    coord_trim_x(sum(sizes$len)) +
+    coord_trim_x(max(breaks)) +
     ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 315, hjust = 0))
 }

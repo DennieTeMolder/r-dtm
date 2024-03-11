9a77a2fb45ee76ce37b8055ade0652e1a3759b92
DennieTeMolder <dennie503@live.nl>
2024-03-11 17:47:05 +0100

    plot_chrom_continuous: don't sort contigs
    This enables the user to control the order of contigs by supplying SIZES
    in the desired plotting order

--- R/pastef.R
+++ R/pastef.R
@@ -1,7 +1,7 @@
 ##'@export
 pastef <- function(..., sep = "") {
   args <- purrr::map_if(list(...), is.numeric, function(x) {
-    format(x, big.mark = ifelse(getOption("OutDec") == ".", ",", "."), trim = TRUE)
+    format(x, big.mark = if (identical(getOption("OutDec"), ".")) "," else ".", trim = TRUE)
   })
   do.call(paste, c(args, sep = sep))
 }
--- R/plot_chrom_continuous.R
+++ R/plot_chrom_continuous.R
@@ -1,12 +1,13 @@
 # This function only provides the canvas, you still need to add a geom_.
 # This function creates/modifies df$.pos_adj and aes$x
+# Additionally if TEXT_AES is true df$.text and aes$x are created/modified
 ##' @export
 ##' @importFrom rlang .data
-plot_chrom_continuous <- function(df, aes = NULL, sizes = NULL) {
+plot_chrom_continuous <- function(df, aes = NULL, sizes = NULL, text_aes = TRUE) {
   # Check input
   stopifnot(is.data.frame(df), c("chrom", "pos") %in% colnames(df))
-  stopifnot(is.character(df$chrom), !is.na(df$chrom))
-  stopifnot(is.numeric(df$pos), !is.na(df$pos))
+  stopifnot(is.character(df$chrom), !anyNA(df$chrom))
+  stopifnot(is.numeric(df$pos), !anyNA(df$pos))
   if (!is.null(sizes)) {
     stopifnot(is.data.frame(sizes), dplyr::between(ncol(sizes), 2, 3))
     stopifnot(is.character(sizes[[1]]), !anyDuplicated(sizes[[1]]))
@@ -23,13 +24,13 @@ plot_chrom_continuous <- function(df, aes = NULL, sizes = NULL) {
   if (is.null(sizes)) {
     # Scaffolds are often larger, but this is a good approximation
     sizes <- dplyr::summarise(dplyr::group_by(df, .data$chrom), len = max(.data$pos))
+    sizes <- arrange(sizes, -len)
   } else {
     colnames(sizes)[1:2] <- c("chrom", "len")
     sizes <- dplyr::filter(dplyr::ungroup(sizes), .data$chrom %in% used_chroms)
   }
 
   # Compute position adjustment
-  sizes <- dplyr::arrange(sizes, -.data$len)
   adjust_pos <- cumsum(utils::head(sizes$len, n = -1))
   adjust_pos <- c(0, adjust_pos)
   names(adjust_pos) <- sizes$chrom
@@ -46,9 +47,15 @@ plot_chrom_continuous <- function(df, aes = NULL, sizes = NULL) {
     aes$x <- ggplot2::aes(x = .data$.pos_adj)$x
   }
 
+  # NOTE aes(text = ...) is included by plotly in the hover label
+  if (text_aes) {
+    df$.text <- pastef(df$pos, "bp ", df$chrom, sep = "")
+    aes$text <- ggplot2::aes(text = .data$.text)$text
+  }
+
   # Plot
   ggplot2::ggplot(df, aes) +
     ggplot2::scale_x_continuous(breaks = breaks, minor_breaks = NULL) +
     coord_trim_x(max(breaks)) +
-    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 315, hjust = 0))
+    ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, hjust = 1, vjust = 1))
 }

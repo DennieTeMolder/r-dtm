e2b80dae3def66a74b3d1f607673771bb67c265a
DennieTeMolder <dennie503@live.nl>
2024-04-26 11:33:17 +0200

    mean_impute: improve speed

--- R/mean_impute.R
+++ R/mean_impute.R
@@ -1,7 +1,7 @@
 ##' @export
 mean_impute <- function(x) {
   is_na <- is.na(x)
-  if (any(is_na))
-    x[is_na] <- mean(x, na.rm = TRUE)
+  if (any(is_na) && !all(is_na))
+    x[is_na] <- mean(x[!is_na])
   x
 }

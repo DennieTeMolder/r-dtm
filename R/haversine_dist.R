943f8637f517a4d86bc4f7e0c728f5a5b5f98baf
DennieTeMolder <dennie503@live.nl>
2024-04-26 11:36:06 +0200

    haversine_dist: new function

--- R/haversine_dist.R
+++ R/haversine_dist.R
@@ -1,6 +1,27 @@
-# Ref: https://stackoverflow.com/a/28372037
 #' @export
-haversine_dist <- function(lon1, lat1, lon2, lat2) {
+haversine_dist <- function(lon, lat, name = NULL) {
+  stopifnot(is.double(lon), is.double(lat), length(lon) == length(lat))
+  if (!is.null(name))
+    stopifnot(length(name) == length(lon))
+
+  len <- length(lon)
+  dnames <- list(name, name)
+  if (len == 1L)
+    return(matrix(0, dimnames = dnames))
+
+  # Compute the comparisons for the lower triangle
+  comps <- seq_len(len)
+  comps <- expand.grid(i = comps, j = comps, KEEP.OUT.ATTRS = FALSE)
+  comps <- comps[comps$i > comps$j, ]
+
+  res <- matrix(0, nrow = len, ncol = len, dimnames = dnames)
+  res[as.matrix(comps)] <- with(comps, .haversine_dist_internal(lon[i], lat[i], lon[j], lat[j]))
+  res[as.matrix(comps[2:1])] <- res[as.matrix(comps)]
+  res
+}
+
+# Ref: https://stackoverflow.com/a/28372037
+.haversine_dist_internal <- function(lon1, lat1, lon2, lat2) {
   dLat <- (lat2 - lat1) * pi / 180
   dLon <- (lon2 - lon1) * pi / 180
   a <- sin(dLat / 2)^2 + cos(lat1 * pi / 180) * cos(lat2 * pi / 180) * sin(dLon / 2)^2

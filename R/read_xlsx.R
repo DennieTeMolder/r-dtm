fb77f4becfc9570730ce03cf1e0c8fd723eaf92c
DennieTeMolder <dennie503@live.nl>
2024-05-01 15:09:59 +0200

    read_xlsx: add `sheet` argument

--- R/read_xlsx.R
+++ R/read_xlsx.R
@@ -1,8 +1,15 @@
 ##' @export
-read_xlsx <- function(file, na = c("", "NA"), ...) {
-  sheets <- readxl::excel_sheets(file)
-  f <- function(sheet)
+read_xlsx <- function(file, sheet = NULL, na = c("", "NA"), ...) {
+  if (is.null(sheet)) {
+    sheets <- readxl::excel_sheets(file)
+  } else {
+    sheets <- sheet
+  }
+
+  f <- function(sheet) {
     readxl::read_excel(file, sheet = sheet, na = na, ...)
+  }
+
   if (length(sheets) < 2)
     return(f(sheets))
   sapply(sheets, f, simplify = FALSE, USE.NAMES = TRUE)

466b43f7429912d8382c3ea01d8863e94b51af32
DennieTeMolder <dennie503@live.nl>
2026-04-15 13:08:38 +0200

    read_gff3: use strings over factors

--- R/read_gff3.R
+++ R/read_gff3.R
@@ -1,6 +1,6 @@
 # See: https://github.com/The-Sequence-Ontology/Specifications/blob/master/gff3.md
 ##' @export
-read_gff3 <- function(file, check = TRUE) {
+read_gff3 <- function(file, check = TRUE, ...) {
   first_line <- readr::read_lines(file, n_max = 1)
   if (check && !stringr::str_starts(first_line, "##gff-version 3"))
     stop("File header does not appear to be GFF3!")
@@ -9,8 +9,9 @@ read_gff3 <- function(file, check = TRUE) {
                 "phase", "attributes")
   readr::read_tsv(file,
     col_names = gff_cols,
-    col_types = "fffiidfic",
+    col_types = "ccciidcic",
     comment = "#",
-    na = c(".", "NA")
+    na = c(".", "NA"),
+    ...
   )
 }
